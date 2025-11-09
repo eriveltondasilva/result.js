f7010f06ec3d9c7bd465b98e4aa8add748d01af9
Erivelton Silva <eriveltondasilva13@gmail.com>
2025-11-08 22:25:28 -0300

    build: add banner to tsup setup

---
+++ src/banner.ts
@@ -0,0 +1,23 @@
+import pkg from '../package.json' with { type: 'json' }
+
+const { name, description, version, author, license, homepage } = pkg
+const year = new Date().getFullYear()
+
+export const banner = `/**
+ * ${name.toUpperCase()} - v${version}
+ *
+ * ${description}
+ *
+ * @author ${author.name} <${author.email}>
+ * @license ${license.toUpperCase()}
+ * @copyright ${year} ${author.name}
+ * @version ${version}
+ *
+ * @see ${homepage} - Documentation
+ *
+ * Inspired by:
+ * @see https://doc.rust-lang.org/std/result - Rust Result Type
+ * @see https://www.npmjs.com/package/oxide.ts - oxide.ts Package
+ * @see https://www.npmjs.com/package/result.ts - result.ts Package
+ */
+`
