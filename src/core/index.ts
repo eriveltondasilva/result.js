a6a13d4f152e3115f03b4f20498f47333c0f6b70
Erivelton Silva <eriveltondasilva13@gmail.com>
2025-11-19 09:43:41 -0300

    refactor: freeze Result object to prevent modifications

--- src/core/index.ts
+++ src/core/index.ts
@@ -5,7 +5,7 @@ import type { AsyncResult as AsyncResultType, Result as ResultType } from './typ
 /**
  * Result factory methods and utilities.
  */
-export const Result = {
+export const Result = Object.freeze({
   ok: factories.createOk,
   err: factories.createErr,
   fromTry: factories.createFromTry,
@@ -17,7 +17,7 @@ export const Result = {
   any: factories.createAny,
   partition: factories.createPartition,
   allSettled: factories.createAllSettled,
-} as const
+} as const)
 
 export type Result<T, E = Error> = ResultType<T, E>
 export type AsyncResult<T, E = Error> = AsyncResultType<T, E>
