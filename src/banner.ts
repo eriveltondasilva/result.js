75aa029d8c647c4d2d971e73cc6c50a98f66dce7
Erivelton Silva <eriveltondasilva13@gmail.com>
2025-12-12 14:55:09 -0300

    docs: reorganize inspiration list in banner

--- src/banner.ts
+++ src/banner.ts
@@ -17,9 +17,8 @@ export const banner = `/**
  *
  * Inspired by:
  * @see https://doc.rust-lang.org/std/result - Rust Result Type
+ * @see https://hexdocs.pm/gleam_stdlib/gleam/result.html - Gleam Result Type
  * @see https://www.npmjs.com/package/oxide.ts - oxide.ts Package
  * @see https://www.npmjs.com/package/result.ts - result.ts Package
- * @see https://hexdocs.pm/gleam_stdlib/gleam/result.html - Gleam Result Type
- * @see https://github.com/gleam-lang/gleam/blob/main/stdlib/result.md - Gleam Result Type
  */
 `
