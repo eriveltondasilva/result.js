a5221ef703509caeed18293d2f8eadb4516ee763
Erivelton Silva <eriveltondasilva13@gmail.com>
2026-04-26 23:50:01 -0300

    move ResultTypeError class to utils.ts and update its constructor

--- src/errors.ts
+++
@@ -1,9 +0,0 @@
-export class ResultTypeError extends TypeError {
-  constructor(message: string, cause: unknown) {
-    super(`${message}.\nMake sure element is created with Result.ok() or Result.err().`, { cause })
-
-    this.name = 'ResultTypeError'
-
-    Object.setPrototypeOf(this, new.target.prototype)
-  }
-}
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,13 @@
+export class ResultTypeError extends TypeError {
+  constructor(message: string, cause?: unknown) {
+    super(`${message}.\nMake sure element is created with Result.ok() or Result.err().`, { cause })
+
+    this.name = 'ResultTypeError'
+
+    Object.setPrototypeOf(this, new.target.prototype)
+  }
+}
+
 export function isEmptyArray(value: unknown): boolean {
   if (!Array.isArray(value)) {
     throw new TypeError(
